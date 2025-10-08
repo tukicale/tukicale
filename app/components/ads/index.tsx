43986a4b15bad9c3d9c22e6e3243c3816264c718
tukicale <tukicale@gmail.com>
2025-10-08 10:13:11 +0900

    index.tsxにexport文を追加

---
+++ app/components/ads/index.tsx
@@ -0,0 +1,29 @@
+import { Ad10s } from './Ad10s';
+import { Ad20s } from './Ad20s';
+import { Ad30s } from './Ad30s';
+import { Ad40s } from './Ad40s';
+import { Ad50sPlus } from './Ad50sPlus';
+import { AdDefault } from './AdDefault';
+
+export const AgeBasedAdCard = () => {
+  const ageGroup = typeof window !== 'undefined' 
+    ? localStorage.getItem('tukicale_age_group') || '' 
+    : '';
+  
+  switch(ageGroup) {
+    case '10代':
+      return <Ad10s />;
+    case '20代':
+      return <Ad20s />;
+    case '30代':
+      return <Ad30s />;
+    case '40代':
+      return <Ad40s />;
+    case '50代以上':
+      return <Ad50sPlus />;
+    default:
+      return <AdDefault />;
+  }
+};
+
+export { BannerAd } from './BannerAd';
