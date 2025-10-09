d55629fac09ca7826320555ddf48cb872a6aeb2a
tukicale <tukicale@gmail.com>
2025-10-09 11:47:41 +0900

    BannerAdをReact記法に修正

--- app/components/ads/BannerAd.tsx
+++ app/components/ads/BannerAd.tsx
@@ -1,9 +1,27 @@
 export const BannerAd = () => {
-  // ★★★ afbコード ★★★
-  
   return (
     <div className="mb-4 flex justify-center">
-      <a href="https://t.afi-b.com/visit.php?a=H14552a-Z503182b&p=m939301A" rel="nofollow"><img src="https://www.afi-b.com/upload_image/14552-1725182806-3.jpg" width="640" height="360" style="border:none;" alt="フィシュル" /></a><img src="https://t.afi-b.com/lead/H14552a/m939301A/Z503182b" width="1" height="1" style="border:none;" />
+      <a 
+        href="https://t.afi-b.com/visit.php?a=H14552a-Z503182b&p=m939301A" 
+        rel="nofollow"
+        target="_blank"
+      >
+        <img 
+          src="https://www.afi-b.com/upload_image/14552-1725182806-3.jpg" 
+          width="640" 
+          height="360" 
+          style={{ border: 'none' }} 
+          alt="フィシュル"
+          className="hover:opacity-90 transition-opacity rounded-lg"
+        />
+      </a>
+      <img 
+        src="https://t.afi-b.com/lead/H14552a/m939301A/Z503182b" 
+        width="1" 
+        height="1" 
+        style={{ border: 'none', display: 'none' }} 
+        alt=""
+      />
     </div>
   );
 };
